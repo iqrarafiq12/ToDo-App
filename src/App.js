d6678c02d812b804e40030bbabb2225e8b1332cb
Iqra Rafiq <123827193+iqrarafiq12@users.noreply.github.com>
2023-04-08 01:30:46 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);  return (
+    <div className="App">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@500;600;700&family=Caveat:wght@500&family=Didact+Gothic&family=PT+Sans+Narrow&family=Rajdhani:wght@500&display=swap" rel="stylesheet" />
+      <h1>To-Do App</h1>
+
+      {/* FOR ALL Input */}
+      <div className="todo-wrapper">
+        <div className='todo-input'>
+
+          {/* Need Icon | For Label & Inputs */}
+          <div className='todo-input-item'>
+            <label> Title </label>
+            <input type="text" placeholder="What's The Task?" />
+          </div>
+
+          <div className='todo-input-item'>
+            <label> Description </label>
+            <input type="text" placeholder="What's The Description?" />
+          </div>
+
+          {/* Buttons */}
+          <div className='todo-input-item'>
+            <button type='button' className='primarybtn'>Add</button>
+          </div>
+          </div>
+          {/* Showing Buttons */}
+          
+          <div className='btn-area'>
+            <button className="seconderybtn">Todo</button>
+            <button className="seconderybtn1">Completed</button>
+          </div>
+
+          {/* List Area */}
+          <div className="todo-list">
+            <div className="todo-list-item">
+              <h1>Task 1</h1>
+              <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
