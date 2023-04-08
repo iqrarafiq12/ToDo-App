27c9ca1dd01ee8de05a88138aaef9871ce4df7b2
Iqra Rafiq <123827193+iqrarafiq12@users.noreply.github.com>
2023-04-09 01:42:38 +0500

    activate the butons of the list

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [isCompleteScreen, setIsCompleteScreen] = useState(false); 
-  
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+
   return (
     <div className="App">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@500;600;700&family=Caveat:wght@500&family=Didact+Gothic&family=PT+Sans+Narrow&family=Rajdhani:wght@500&display=swap" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@500;600;700&family=Caveat:wght@500&family=Didact+Gothic&family=PT+Sans+Narrow&family=Rajdhani:wght@500&display=swap" rel="stylesheet" />
+      
+      {/* Heading */}
       <h1>To-Do App</h1>
 
       {/* FOR ALL Input */}
       <div className="todo-wrapper">
-        <div className='todo-input'>
+      <div className='todo-input'>
 
           {/* Need Icon | For Label & Inputs */}
           <div className='todo-input-item'>
@@ -30,24 +32,27 @@ function App() {
           <div className='todo-input-item'>
             <button type='button' className='primarybtn'>Add</button>
           </div>
-          </div>
-          {/* Showing Buttons */}
-          
-          <div className='btn-area'>
-            <button className={`isCompleteScreen ${isCompleteScreen===false && 'active' } `} onClick={()=> setIsCompleteScreen(false)}>Todo</button>
-            <button className={`isCompleteScreen ${isCompleteScreen===true && 'active' } `} onClick={()=> setIsCompleteScreen(true)}>Completed</button>
-          </div>
+        </div>
 
-          {/* List Area */}
-          <div className="todo-list">
-            <div className="todo-list-item">
-              <h1>Task 1</h1>
-              <p>Description</p>
+        {/* Showing Buttons */}
+        <div className='btn-area'>
+          <button className={`seconderyBtn ${isCompleteScreen === false && 'active'} `} onClick={() => setIsCompleteScreen(false)} >Todo</button>
+          <button className={`seconderyBtn1 ${isCompleteScreen === true && 'active'} `} onClick={() => setIsCompleteScreen(true)} >Completed</button>
+        </div>
+
+{/*  List */}
+<div className='todo-list'>
+
+        <div className="todo-list-item">
+            <h1>Task 1</h1>
+            <p>Description</p>
           </div>
         </div>
+
       </div>
     </div>
   );
+
 }
 
 export default App;
